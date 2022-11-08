a221f6bec503ba5f3d33ebdf41ac363f0abe0af6
D Ther <dtherhtun.cw@gmail.com>
2022-11-09 01:36:24 +0700

    feat(8.4): Token Signature Validation

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
 	"io"
@@ -71,13 +72,13 @@ func genToken() error {
 	token := jwt.NewWithClaims(method, claims)
 	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
 
-	str, err := token.SignedString(privateKey)
+	tokenStr, err := token.SignedString(privateKey)
 	if err != nil {
 		return err
 	}
 
 	fmt.Println("========== TOKEN BEGIN ==========")
-	fmt.Println(str)
+	fmt.Println(tokenStr)
 	fmt.Println("========== TOKEN END ==========")
 	fmt.Print("\n")
 
@@ -97,6 +98,50 @@ func genToken() error {
 	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
 		return fmt.Errorf("encoding to public file: %w", err)
 	}
+
+	// ======================================================================
+
+	fmt.Println("=======================")
+	// Create the token parser to use. The algorithm used to sign the JWT must be
+	// validated to avoid a critical vulnerability:
+	// http://auth0.com/blog/critical-vulnerabilities-in-json-web-token-libraries
+	//parser := jwt.Parser{
+	//	ValidMethods: []string{"RS256"},
+	//}
+
+	option := jwt.WithValidMethods([]string{"RS256"})
+
+	parser := jwt.NewParser(option)
+
+	keyFunc := func(t *jwt.Token) (interface{}, error) {
+		kid, ok := t.Header["kid"]
+		if !ok {
+			return nil, errors.New("missing key id (kid) in token header")
+		}
+		kidID, ok := kid.(string)
+		if !ok {
+			return nil, errors.New("user token key id (kid) must be string")
+		}
+		fmt.Println("KID:", kidID)
+
+		return &privateKey.PublicKey, nil
+	}
+
+	var parsedClaims struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}
+
+	parsedToken, err := parser.ParseWithClaims(tokenStr, &parsedClaims, keyFunc)
+	if err != nil {
+		return fmt.Errorf("parsing token: %w", err)
+	}
+
+	if !parsedToken.Valid {
+		return errors.New("invalid token")
+	}
+
+	fmt.Println("Token Validate")
 
 	return nil
 }

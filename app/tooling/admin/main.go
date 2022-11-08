196aa836221e0b2213668c9f9e2899f5bd57d9bc
D Ther <dtherhtun.cw@gmail.com>
2022-11-08 23:44:23 +0700

    feat(8.3): Token Generation

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -6,17 +6,102 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"io"
 	"os"
+	"time"
 )
 
 func main() {
-	err := getKey()
+	err := genToken()
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func getKey() error {
+func genToken() error {
+
+	name := "zarf/keys/54bb2165-71e1-41a6-af3e-7da4a0e1e2c1.pem"
+	f, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// limit PEM file size to 1 megabyte. This should be reasonable for
+	// almost any PEM file and prevents shenanigans like linking the file
+	// to /dev/random or something like that.
+	privatePEM, err := io.ReadAll(io.LimitReader(f, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+	}
+
+	// =======================================================================
+
+	// Generating a token requires defining a set of the claims. In this applications
+	// case, we only care about defining the subject and the user in question and
+	// the roles they have on the database. This token will expire in a year.
+	//
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only one time)
+	claims := struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "service project",
+			Subject:   "123456789",
+			ExpiresAt: &jwt.NumericDate{Time: time.Now().Add(8760 * time.Hour)},
+			IssuedAt:  &jwt.NumericDate{Time: time.Now().UTC()},
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("========== TOKEN BEGIN ==========")
+	fmt.Println(str)
+	fmt.Println("========== TOKEN END ==========")
+	fmt.Print("\n")
+
+	// =====================================================================
+
+	// Marshal the public key from the private key to PKIX
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshalling public key: %w", err)
+	}
+
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	return nil
+}
+
+func genKey() error {
 
 	// Generate new private key.
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)

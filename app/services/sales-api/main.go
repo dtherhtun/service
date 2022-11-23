475f616ae220658dd5d73390f16e01779bf73b5a
D Ther <dtherhtun.cw@gmail.com>
2022-11-23 20:25:44 +0700

    feat(13.2): Support for Starting and Stopping a Unit Test

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -15,12 +15,12 @@ import (
 	conf "github.com/ardanlabs/conf/v3"
 	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 
 	"github.com/dtherhtun/service/app/services/sales-api/handlers"
 	"github.com/dtherhtun/service/business/sys/auth"
 	"github.com/dtherhtun/service/business/sys/database"
 	"github.com/dtherhtun/service/foundation/keystore"
+	"github.com/dtherhtun/service/foundation/logger"
 )
 
 /*
@@ -33,7 +33,7 @@ var build = "develop"
 func main() {
 
 	// Construct the application logger.
-	log, err := initLogger("SALES-API")
+	log, err := logger.New("SALES-API")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -238,21 +238,4 @@ func run(log *zap.SugaredLogger) error {
 	}
 
 	return nil
-}
-
-func initLogger(service string) (*zap.SugaredLogger, error) {
-	config := zap.NewProductionConfig()
-	config.OutputPaths = []string{"stdout"}
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	config.DisableStacktrace = true
-	config.InitialFields = map[string]interface{}{
-		"service": service,
-	}
-
-	log, err := config.Build()
-	if err != nil {
-		return nil, err
-	}
-
-	return log.Sugar(), nil
 }
